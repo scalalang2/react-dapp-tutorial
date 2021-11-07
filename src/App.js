563997a856e417db288b2cfce08c3c8bef6521f5
scalalang2 <cailhuiris@gmail.com>
2021-11-08 00:06:28 +0900

    Web3 설치 및 지갑 연동 예시

--- src/App.js
+++ src/App.js
@@ -27,62 +27,72 @@ import Call from './example/wallet/Call';
 import Connect from './example/wallet/Connect';
 import Execute from './example/wallet/Execute';
 import DefaultData from './example/react/DefaultData';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+
+function getLibrary(provider) {
+    const library = new Web3Provider(provider)
+    library.pollingInterval = 12000
+    return library
+}
 
 function App() {
     return (
-        <Router>
-            <div className="app">
-                <Header />
-                <EuiPage paddingSize="none" style={{ height: '100%'}}>
-                    <EuiPageSideBar>
-                        <SideNav />
-                    </EuiPageSideBar>
-                    <EuiPageBody panelled paddingSize="none">
-                        <EuiPageBody paddingSize="m">
-                            <EuiPageContent
-                                hasBorder={false}
-                                hasShadow={false}
-                                paddingSize="none"
-                                color="transparent"
-                                borderRadius="none"
-                            >
-                                <EuiPageContentBody>
-                                    <Switch>
-                                        <Route path="/example/data">
-                                            <DefaultData />
-                                        </Route>
-                                        <Route path="/example/counter">
-                                            <Counter />
-                                        </Route>
-                                        <Route path="/example/sync-input">
-                                            <SyncInput />
-                                        </Route>
-                                        <Route path="/example/sync-array">
-                                            <SyncArray />
-                                        </Route>
-                                        <Route path="/example/sync-external">
-                                            <SyncExternal/>
-                                        </Route>
-                                        <Route path="/wallet/connect">
-                                            <Connect />
-                                        </Route>
-                                        <Route path="/wallet/call">
-                                            <Call />
-                                        </Route>
-                                        <Route path="/wallet/execute">
-                                            <Execute />
-                                        </Route>
-                                        <Route exact path="/">
-                                            home
-                                        </Route>
-                                    </Switch>
-                                </EuiPageContentBody>
-                            </EuiPageContent>
+        <Web3ReactProvider getLibrary={getLibrary}>
+            <Router>
+                <div className="app">
+                    <Header />
+                    <EuiPage paddingSize="none" style={{ height: '100%'}}>
+                        <EuiPageSideBar>
+                            <SideNav />
+                        </EuiPageSideBar>
+                        <EuiPageBody panelled paddingSize="none">
+                            <EuiPageBody paddingSize="m">
+                                <EuiPageContent
+                                    hasBorder={false}
+                                    hasShadow={false}
+                                    paddingSize="none"
+                                    color="transparent"
+                                    borderRadius="none"
+                                >
+                                    <EuiPageContentBody>
+                                        <Switch>
+                                            <Route path="/example/data">
+                                                <DefaultData />
+                                            </Route>
+                                            <Route path="/example/counter">
+                                                <Counter />
+                                            </Route>
+                                            <Route path="/example/sync-input">
+                                                <SyncInput />
+                                            </Route>
+                                            <Route path="/example/sync-array">
+                                                <SyncArray />
+                                            </Route>
+                                            <Route path="/example/sync-external">
+                                                <SyncExternal/>
+                                            </Route>
+                                            <Route path="/wallet/connect">
+                                                <Connect />
+                                            </Route>
+                                            <Route path="/wallet/call">
+                                                <Call />
+                                            </Route>
+                                            <Route path="/wallet/execute">
+                                                <Execute />
+                                            </Route>
+                                            <Route exact path="/">
+                                                home
+                                            </Route>
+                                        </Switch>
+                                    </EuiPageContentBody>
+                                </EuiPageContent>
+                            </EuiPageBody>
                         </EuiPageBody>
-                    </EuiPageBody>
-                </EuiPage>
-            </div>
-        </Router>
+                    </EuiPage>
+                </div>
+            </Router>
+        </Web3ReactProvider>
     );
 }
 
--- src/example/wallet/Connect.js
+++ src/example/wallet/Connect.js
@@ -1,14 +1,39 @@
-import { EuiFlexGroup, EuiFlexItem } from "@elastic/eui";
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { useWeb3React } from '@web3-react/core'
+import { EuiFlexGroup, EuiFlexItem, EuiButton } from "@elastic/eui";
+import { useEffect, useState } from 'react';
+
+const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] })
 
 function Connect() {
+    const context = useWeb3React();
+    const { connector, library, chainId, account, activate, deactivate, active, error } = context;
+    const [balance, setBalance] = useState("0");
+
+    useEffect(() => {
+        if(!!account && !!library) {
+            library.getBalance(account).then(value => {
+                console.log(value);
+                setBalance(value.toString());
+            })
+        }
+    }, [account, library, chainId])
+
+    const connectWallet = () => {
+        activate(injected);
+    }
+    
     return (
-        <EuiFlexGroup>
-            <EuiFlexItem grow={4}>
-                <EuiFlexGroup direction='column'>
-                    <EuiFlexItem>
-                        
-                    </EuiFlexItem>
-                </EuiFlexGroup>
+        <EuiFlexGroup direction={'column'} style={{ width: '30%' }}>
+            <EuiFlexItem>
+                <EuiButton onClick={() => connectWallet() }>지갑 연결하기</EuiButton>
+            </EuiFlexItem>
+            <EuiFlexItem>
+                {active && (<>{"당신의 지갑 주소는 : " + account + " 입니다."}</>) }
+                {!active && (<>지갑을 연결해주세요.</>)}
+            </EuiFlexItem>
+            <EuiFlexItem>
+                { balance } ETH
             </EuiFlexItem>
         </EuiFlexGroup>
     )
